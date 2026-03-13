9612372b04dc16936e6ce8167020203bf197b34f
KondratkoVK <VladiclavXXX@mail.ru>
2026-03-13 10:19:39 +0000

    Update detection scripts and monitoring configuration

--- detection/ssh_bruteforce.sh
+++ detection/ssh_bruteforce.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
 LOG="/var/log/auth.log"
-METRIC_FILE="/home/creator/linux-security-monitor/metrics/ssh_bruteforce.prom"
 
-ATTEMPTS=$(grep "Failed password" $LOG | tail -n 100 | wc -l)
+# путь к папке скрипта
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo "# HELP ssh_failed_logins Number of failed SSH login attempts" > $METRIC_FILE
-echo "# TYPE ssh_failed_logins gauge" >> $METRIC_FILE
-echo "ssh_failed_logins $ATTEMPTS" >> $METRIC_FILE
+# папка metrics относительно проекта
+METRICS_DIR="$SCRIPT_DIR/../metrics"
+mkdir -p "$METRICS_DIR"
+
+# файл метрик
+METRIC_FILE="$METRICS_DIR/ssh_bruteforce.prom"
+
+NOW=$(date +%s)
+WINDOW=120
+
+echo "# HELP ssh_failed_logins Failed SSH login attempts per IP in last 2 minutes" > "$METRIC_FILE"
+echo "# TYPE ssh_failed_logins gauge" >> "$METRIC_FILE"
+
+tail -n 500 "$LOG" | grep "Failed password" | while read line
+do
+    LOG_TIME=$(echo "$line" | awk '{print $1}')
+    LOG_TIMESTAMP=$(date -d "$LOG_TIME" +%s 2>/dev/null)
+
+    if [ -z "$LOG_TIMESTAMP" ]; then
+        continue
+    fi
+
+    if [ $((NOW - LOG_TIMESTAMP)) -le $WINDOW ]; then
+        IP=$(echo "$line" | grep -oP 'from \K[0-9.]+')
+        echo "$IP"
+    fi
+
+done | sort | uniq -c | while read count ip
+do
+    echo "ssh_failed_logins{ip=\"$ip\"} $count" >> "$METRIC_FILE"
+done
